df5fb54e0faac7d829e3b9d688a71c62f1255f4e
Angelica Li <liyunshuangelica@gmail.com>
2024-10-26 16:50:16 -0500

    added info popup card with placeholder text

--- src/components/VideoHeader.jsx
+++ src/components/VideoHeader.jsx
@@ -1,17 +1,56 @@
+import {useState} from "react";
 import { IconButton, Typography } from "@mui/material";
 import DeleteIcon from '@mui/icons-material/Delete';
+import InfoIcon from '@mui/icons-material/Info';
 import IconButton from '@mui/material/IconButton';
+import { Popper } from '@mui/base/Popper';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 
 export default function VideoHeader({ timestamp, videoId, adminMode, onDelete }) {
+    const [anchor, setAnchor] = useState(false);
+
+    const handleInfoClick = (event) => {
+        setAnchor(anchor ? null : event.currentTarget);
+    };
     return (
-        <div className="video-header" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-            <Typography margin='0.2em'>{timestamp}</Typography>
-            {adminMode && <IconButton
-                aria-label="delete"
-                size="small"
-                onClick={() => onDelete(videoId)}>
-                <DeleteIcon fontSize="inherit" />
-            </IconButton>}
-        </div>
+        <>
+            <div className="video-header" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Typography margin='0.2em' style={{ flexGrow: 1 }}>{timestamp}</Typography>
+                {adminMode && (<>
+                    <IconButton
+                        aria-label="delete"
+                        size="small"
+                        onClick={() => onDelete(videoId)}
+                        color="warning">
+                        <DeleteIcon fontSize="inherit" />
+                    </IconButton>
+                    <IconButton
+                        aria-label="info"
+                        size="small"
+                        onClick={handleInfoClick}>
+                        <InfoIcon fontSize="inherit" />
+                    </IconButton>
+                </>
+                )}
+            </div>
+            <Popper id={videoId + '-info'} open={Boolean(anchor)} anchorEl={anchor} placement="right">
+                <Card>
+                    <CardContent style={{ maxHeight: '200px', maxWidth: '300px', overflowY: 'auto' }}>
+                    long text that may exceed the height of the card content...
+                    long text that may exceed the height of the card content...
+                    long text that may exceed the height of the card content...
+                    long text that may exceed the height of the card content...
+                    long text that may exceed the height of the card content...
+                    long text that may exceed the height of the card content...
+                    long text that may exceed the height of the card content...
+                    long text that may exceed the height of the card content...
+                    long text that may exceed the height of the card content...
+                    long text that may exceed the height of the card content...
+                    long text that may exceed the height of the card content...
+                    </CardContent>
+                </Card>
+            </Popper>
+        </>
     )
 }
